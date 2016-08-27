753f4afa4adac43940d7cab0de796aa99a9f24e9
Hunter Campbell <hcnureth@gmail.com>
2016-08-26 19:27:20 -0600

    Upload HuntersResumeJQ

---
+++ HuntersResumeJQ.js
@@ -0,0 +1,14 @@
+var main = function() {
+	
+	var pass = prompt("What is Hunter's resume password?").toUpperCase();
+	
+	if(pass === "JS2X31") {
+		$("html").show();
+	}
+	else {
+		$("html").remove();
+		alert("Access Denied.\nIf you want access, contact Hunter for the password.");
+	};
+};
+
+$(document).ready(main);
