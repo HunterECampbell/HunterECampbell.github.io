fa7cd25fd0c67537a1ac5eee57f3a7a1743b23fe
Hunter Campbell <hcnureth@gmail.com>
2017-05-26 21:41:35 -0600

    Add personalized comments
    Comments added on lines 7, 13, and 22.

--- ggplot2_titanic_exercise.R
+++ ggplot2_titanic_exercise.R
@@ -4,11 +4,13 @@
 str(titanic)
 
 # Use ggplot() for the first instruction
+#making a bar plot with titanic, having the bars side by side to show survival rates of each gender and class
 ggplot(titanic, aes(x = factor(Pclass), fill = factor(Sex))) +
   geom_bar(position = "dodge")
 
 
 # Use ggplot() for the second instruction
+#Make a facet grid from the previous bar plot
 ggplot(titanic, aes(x = factor(Pclass), fill = factor(Sex))) +
   geom_bar(position = "dodge") +
   facet_grid(". ~ Survived")
@@ -17,6 +19,7 @@ ggplot(titanic, aes(x = factor(Pclass), fill = factor(Sex))) +
 posn.j <- position_jitter(0.5, 0)
 
 # Use ggplot() for the last instruction
+#Show a scatter plot with the previous bar plot/facet grid
 ggplot(titanic, aes(x = factor(Pclass), y = Age, col = factor(Sex))) +
   geom_jitter(size = 3, alpha = 0.5, position = posn.j) +
   facet_grid(". ~ Survived")
