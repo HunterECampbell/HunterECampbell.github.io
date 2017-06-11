1a903bee3b9da9845d5bc787563259f359fdba62
Hunter Campbell <hcnureth@gmail.com>
2017-06-10 23:46:45 -0600

    Upload refine project
    data wrangling assignment for Springboard Data Science course

--- refine_project/refine_clean.R
+++ refine_project/refine_clean.R
@@ -1,32 +1,43 @@
+#load libraries
+install.packages("tidyr")
+install.packages("dplyr")
+library("tidyr")
+library("dplyr")
+
+#load csv file
+refine_original <- read.csv("C:/Users/hcnur_000/OneDrive/Documents/refine_project/refine_original.csv")
+
 #this was to alphabetize the company column
-refine_original <- refine_original %>% arrange(company)
+refine_clean <- refine_original %>% arrange(company)
 
 #this was to make the company column more readable
-refine_original$company <- gsub("akzo|ak zo|akz0", "akzo", refine_original$company, ignore.case = TRUE)
-refine_original$company <- gsub("fillips|philips|phillips|phlips|phillps|phllips", "philips", refine_original$company, ignore.case = TRUE)
-refine_original$company <- gsub("unilever|unilver", "unilever", refine_original$company, ignore.case = TRUE)
-refine_original$company <- gsub("van houten", "van houten", refine_original$company, ignore.case = TRUE)
+refine_clean$company <- gsub("akzo|ak zo|akz0", "akzo", refine_clean$company, ignore.case = TRUE)
+refine_clean$company <- gsub("fillips|philips|phillips|phlips|phillps|phllips", "philips", refine_clean$company, ignore.case = TRUE)
+refine_clean$company <- gsub("unilever|unilver", "unilever", refine_clean$company, ignore.case = TRUE)
+refine_clean$company <- gsub("van houten", "van houten", refine_clean$company, ignore.case = TRUE)
 
 #making two columns out of "Product code / number"
-refine_original <- separate(refine_original, 'Product code / number', c("product_code", "product_number"), sep = "-")
+refine_clean <- separate(refine_clean, Product.code...number, c("product_code", "product_number"), sep = "-")
 
 #making a product categories column
-refine_original <- mutate(refine_original, product_category = ifelse(refine_original$product_code == "p", "Smartphone", ifelse(refine_original$product_code == "v", "TV", ifelse(refine_original$product_code == "x", "Laptop", "Tablet"))))
+refine_clean <- mutate(refine_clean, product_category = ifelse(refine_clean$product_code == "p", "Smartphone", ifelse(refine_clean$product_code == "v", "TV", ifelse(refine_clean$product_code == "x", "Laptop", "Tablet"))))
 
 #moving the product categories column next to the product code and product number columns
-refine_original <- refine_original[c("company", "product_category", "product_code", "product_number", "address", "city", "country", "name")]
+refine_clean <- refine_clean[c("company", "product_category", "product_code", "product_number", "address", "city", "country", "name")]
 
 #making one address column
-refine_original <- unite(refine_original, "full_address", address, city, country, sep = ", ")
+refine_clean <- unite(refine_clean, "full_address", address, city, country, sep = ", ")
 
 #making dummy binary variables for each company
-refine_original <- mutate(refine_original, company_akzo = ifelse(company == "akzo", 1, 0))
-refine_original <- mutate(refine_original, company_philips = ifelse(company == "philips", 1, 0))
-refine_original <- mutate(refine_original, company_unilever = ifelse(company == "unilever", 1, 0))
-refine_original <- mutate(refine_original, company_van_houten = ifelse(company == "van houten", 1, 0))
+refine_clean <- mutate(refine_clean, company_akzo = ifelse(company == "akzo", 1, 0))
+refine_clean <- mutate(refine_clean, company_philips = ifelse(company == "philips", 1, 0))
+refine_clean <- mutate(refine_clean, company_unilever = ifelse(company == "unilever", 1, 0))
+refine_clean <- mutate(refine_clean, company_van_houten = ifelse(company == "van houten", 1, 0))
 
 #making dummy binary variables for each product category
-refine_original <- mutate(refine_original, product_smartphone = ifelse(product_category == "Smartphone", 1, 0))
-refine_original <- mutate(refine_original, product_tv = ifelse(product_category == "TV", 1, 0))
-refine_original <- mutate(refine_original, product_laptop = ifelse(product_category == "Laptop", 1, 0))
-refine_original <- mutate(refine_original, product_tablet = ifelse(product_category == "Tablet", 1, 0))
+refine_clean <- mutate(refine_clean, product_smartphone = ifelse(product_category == "Smartphone", 1, 0))
+refine_clean <- mutate(refine_clean, product_tv = ifelse(product_category == "TV", 1, 0))
+refine_clean <- mutate(refine_clean, product_laptop = ifelse(product_category == "Laptop", 1, 0))
+refine_clean <- mutate(refine_clean, product_tablet = ifelse(product_category == "Tablet", 1, 0))
+
+write.csv(refine_clean, "C:/Users/hcnur_000/OneDrive/Documents/refine_project/refine_clean.csv")
