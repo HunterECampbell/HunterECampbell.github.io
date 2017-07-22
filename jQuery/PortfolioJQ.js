34c271509b954a8b803e5a77d6240da89aa850f8
Hunter Campbell <hcnureth@gmail.com>
2017-07-22 17:50:16 -0600

    Update design

--- jQuery/PortfolioJQ.js
+++ jQuery/PortfolioJQ.js
@@ -35,14 +35,45 @@ var main = function() {
 	
 	$("#more-btn").click(function() {
 		$(".more-websites").slideToggle("slow");
-		$(this).toggleClass("active");
-		if ($(this).text() == "Less...") {
-			$(this).text("More...");
-		}
-		else {
-			$(this).text("Less...");
-		};
+		$("#more-btn").hide();
+		$("#more-btn2").show();
 	});
+	$("#more-btn2").click(function() {
+		$(".more-websites").slideToggle("slow");
+		$("#more-btn").show();
+		$("#more-btn2").hide();
+	});
+	
+	/*$("#goblin").mouseenter(function() {
+		$("#goldSpan").css("color", "white");
+	});
+	$("#goblin").mouseleave(function() {
+		$("#goldSpan").css("color", "#FFD700");
+	});
+	$("#goblin").mouseenter(function() {
+		$("#goblinSpan").css("color", "white");
+	});
+	$("#goblin").mouseleave(function() {
+		$("#goblinSpan").css("color", "#41924b");
+	});
+	$("#RPS").mouseenter(function() {
+		$("#rockSpan").css("color", "white");
+	});
+	$("#RPS").mouseleave(function() {
+		$("#rockSpan").css("color", "brown");
+	});
+	$("#RPS").mouseenter(function() {
+		$("#scissorsSpan").css("color", "white");
+	});
+	$("#RPS").mouseleave(function() {
+		$("#scissorsSpan").css("color", "#7b9095");
+	});
+	$("#dragon").mouseenter(function() {
+		$("#dragonSpan").css("color", "white");
+	});
+	$("#dragon").mouseleave(function() {
+		$("#dragonSpan").css("color", "red");
+	});*/
 };
 
 $(document).ready(main);
