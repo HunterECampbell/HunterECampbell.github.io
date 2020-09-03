b15d5de54bc622a891f837a856328fe42f4c184c
Hunter Campbell <hunter.campbell@printerlogic.com>
2020-09-03 11:11:06 -0600

    Update goblin game

--- js/Goblin.js
+++ js/Goblin.js
@@ -1,6 +1,10 @@
 var main = function() {
 	$("#goblin").click(function() {
 		var user = prompt("You see a goblin carrying a pouch of gold.  Do you FIGHT him, STEAL the pouch from him, or do NOTHING?").toUpperCase();
+    
+    while (user !== 'FIGHT' && user !== 'STEAL' && user !== 'NOTHING') {
+      user = prompt("I don't recognize that response.  Do you FIGHT him, STEAL the pouch from him, or do NOTHING?").toUpperCase();
+    }
 
 		switch(user) {
 			case "FIGHT":
@@ -10,7 +14,7 @@ var main = function() {
 					confirm("The battle somehow goes in your favor!  You hit the goblin's dagger out of his hand and quickly cut off his head.  You pick up your trophy -- the pouch of gold -- and yell a victory cry to the world!");    
 				}
 				else if (strong === "YES" && injured === "NO")
-					confirm("You easily overpower the goblin with your brute strength by slicing off its head!!  You then pick up your trophy -- the pouch of gold -- and yell a victory cry to the world!");
+					confirm("You easily overpower the goblin with your brute strength by slicing off its head!  You then pick up your trophy -- the pouch of gold -- and yell a victory cry to the world!");
 				else if (strong === "NO" && injured === "YES") {
 					confirm("You cripple!  Why would you make such an unwise choice!? The goblin ran you through with his dagger and got away with the pouch of gold.");    
 				}
@@ -22,7 +26,7 @@ var main = function() {
 				};
 				break;
 			case "STEAL":
-				var swift = prompt("Are you swift? YES or NO").toUpperCase();
+				var swift = prompt("Are you swift? (YES or NO)").toUpperCase();
 				var stealthy = prompt("Are you stealthy?").toUpperCase();
 				if (swift === "YES" && stealthy === "YES") {
 					confirm("You are very skilled when it comes to stealing things.  Without the goblin noticing your presence or that the pouch of gold is gone, you run off with the gold in hand!");    
@@ -34,7 +38,7 @@ var main = function() {
 					confirm("You take your time, but because of your patience, the goblin doesn't notice you and you get away with the pouch of gold!");    
 				}
 				else if (swift === "NO" && stealthy === "NO") {
-					confirm("What!?  You have no theft skills at all!!  The goblin notices you and hits you to the ground!  He quickly runs off without a trace, and you lose the pouch of gold.");
+					confirm("What!?  You have no theft skills at all!  The goblin notices you and hits you to the ground!  He quickly runs off without a trace, and you lose the pouch of gold.");
 				}
 				else {
 					confirm("I don't recognize your response.  By your lack of communication, the goblin gets away with the gold.");
@@ -60,7 +64,7 @@ var main = function() {
 				};
 				break;
 			default:
-				confirm("I don't recognize that response.  It will be considered as you doing nothing.");
+        break
 			};
 	});
 };
